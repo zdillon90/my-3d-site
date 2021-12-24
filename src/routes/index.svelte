72f799f4fac2d70c0baf193e6727a62a2008a9e9
Zach Dillon <zdillon90@gmail.com>
2021-12-24 21:49:15 +0000

    it now rotates

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,7 +3,8 @@
     import * as SC from 'svelte-cubed';
 </script>
 
-<SC.Canvas>
+<SC.Canvas antialias background={new THREE.Color('papayawhip')}>
     <SC.Mesh geometry={new THREE.BoxGeometry()} />
     <SC.PerspectiveCamera position={[1, 1, 3]} />
+    <SC.OrbitControls enableZoom={false} />
 </SC.Canvas>
